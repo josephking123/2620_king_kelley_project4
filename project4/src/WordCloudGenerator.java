d659c02a7e97157269c4b0d90d1b812b1faba89c
Sean Kelley <kelleysm@merrimack.edu>
2024-04-18 10:22:45 -0400

    WordCloudGenerator Finished

--- project4/src/WordCloudGenerator.java
+++ project4/src/WordCloudGenerator.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,9 +13,17 @@ public class WordCloudGenerator {
 
     public WordCloudGenerator(Map<String,Integer> map, Set<String> words){
         Iterator<String> it = words.iterator();
+        List<WordFrequency> list = new ArrayList<WordFrequency>();
         while(it.hasNext() == true){
-            
+            String word = it.next();
+            WordFrequency freq = new WordFrequency(word, map.get(word));
+            list.add(freq);
         }
+        cloud.build(list);
+    }
+
+    public WordCloud getCloud() {
+        return cloud;
     }
 
 }
